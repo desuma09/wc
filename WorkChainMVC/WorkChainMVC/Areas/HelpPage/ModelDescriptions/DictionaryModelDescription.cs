32e9a2e2a963b3503d271be2605e938d9197c263
desuma09 <umang@intelworkshop.com>
2018-01-18 10:45:28 +0000

    base

---
+++ WorkChainMVC/WorkChainMVC/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WorkChainMVC.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WorkChainMVC/WorkChainMVC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WorkChainMVC.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
